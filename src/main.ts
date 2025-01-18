de785dae82410a49ab84161fe73a4baf89cf0da4
mhdjok98 <mhdjok@gmail.com>
2025-01-18 23:02:04 +0400

    Add self-signed certificate and update configurations

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,43 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+import fs from 'fs';
+import https from 'https';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // HTTPS Options with self-signed certificates
+  const httpsOptions = {
+    key: fs.readFileSync('selfsigned.key'),
+    cert: fs.readFileSync('selfsigned.crt'),
+  };
+
+  // Enable validation pipes
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // Strip out properties not defined in the DTO
-      forbidNonWhitelisted: true, // Throw an error if unknown properties are passed
-      transform: true, // Automatically transform payloads to match DTO classes
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
+  // Enable CORS
   app.enableCors({
-    // origin: 'http://localhost:3001',
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(3000);
+  // Create HTTPS server
+  const server = https.createServer(
+    httpsOptions,
+    app.getHttpAdapter().getInstance(),
+  );
+
+  server.listen(3000, () => {
+    console.log('Server running on https://localhost:3000');
+  });
 }
+
 bootstrap();
