6adb01de393d5dfeb035a3ba76170ec08dd7f278
mhdjok98 <mhdjok@gmail.com>
2025-01-18 22:49:29 +0400

    change url

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 
   app.enableCors({
     // origin: 'http://localhost:3001',
-    origin: 'itsapple-front.vercel.app',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
